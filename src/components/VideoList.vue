e179663b23fee150ab7c1e347ec055e383d7eb77
unknown <mr.law.jason@gmail.com>
2020-08-19 11:28:52 -0600

    key missing

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,14 +1,20 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length }}
+        <VideoListItem v-for="video in videos">
+        
+        <VideoListItem/>
     </ul>
     
 </template>
 
 <script>
+import VideoListItem from './VideoListItem'
+
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     props: ['videos']
 }
 </script>
