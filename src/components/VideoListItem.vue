22c894350651cf2e37f2e15f0c29a711513980d8
unknown <mr.law.jason@gmail.com>
2020-08-21 19:37:19 -0600

    styling

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,7 +1,7 @@
 <template>
-    <li class="list-group-item">
-        <img :src="video.snippet.thumbnails.default.url" />
-        <div v-html="video.snippet.title">
+    <li class="list-group-item media" @click="onVideoSelect">
+        <img class="mr-3" :src="thumbnailUrl"/>
+        <div  class="media-body" v-html="video.snippet.title">
         </div>
     </li>
 
@@ -10,6 +10,26 @@
 <script>
 export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+
+    computed: {
+        thumbnailUrl() {
+            return this.video.snippet.thumbnails.default.url
+        }
+    },
+    methods: {
+        onVideoSelect() {
+            
+        }
+    }
 }
 </script>
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #eee;
+    }
+</style>
