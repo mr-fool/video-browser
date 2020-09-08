27ef96250206de8b4b681ac1e65f185e059f2f60
unknown <mr.law.jason@gmail.com>
2020-09-08 16:36:05 -0600

    Update VideoDetail.vue

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
     <div v-if="video">
+            <div>
+                <iframe :src="videoUrl" />
+            </div>
             <div class="details">
                 <h4 v-html=video.snippet.title> </h4>
                 <p v-html=video.snippet.description></p>
@@ -10,6 +13,20 @@
 <script>
 export default {
     name: 'VideoDetail',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        videoUrl() {
+            const {videoId} = this.video.id;
+            return `https://www.youtube.com/embed/${videoId}` 
+        }
+    }
 }
 </script>
+<style scoped>
+    .details {
+        margin-top: 10px;
+        padding: 10px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+</style>
